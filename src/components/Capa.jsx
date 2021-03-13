53226fdb15a131a8b35785c34a5668469fcccd0f
AndersonPaim <42255485+AndersonPaim@users.noreply.github.com>
2021-03-12 22:39:23 -0300

    update

--- src/components/Capa.jsx
+++ src/components/Capa.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Typed from 'react-typed';
 export default props => 
    
-    <div className={"capaFundo"}>     
+    <div className={"capaFundo"}> 
+    {/*   
         <ul id="menu">  
             <li><span><p>Portfolio</p></span> </li>
             <a href={"#Projetos"}><li><span><p>Projetos</p></span> </li></a>
             <a href={"#Sobre"}><li><span><p>Sobre</p></span> </li></a>
             <a href={"#Contato"}><li><span><p>Contato</p></span> </li></a>
-        </ul>  
+    </ul>  */} 
         <img className={"fotoPerfil"} src={props.fotoPerfil}/>
         <h1 className={"textoCapa"}>{props.nome}</h1>
         <Typed className={"textoCapa2"}
