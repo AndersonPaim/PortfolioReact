9f8fa5e0da74a71ad1e45b94a85a6487b761d4a1
AndersonPaim <42255485+AndersonPaim@users.noreply.github.com>
2021-03-18 04:08:45 -0300

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => 
+    <div>
+        <div className={"topico"}>     
+            <h2>{"Anderson Paim 2021 - Desenvolvido com React"}</h2>    
+        </div>
+    </div>
+
+
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Capa from './components/Capa.jsx'
 import Topico from './components/Topico.jsx'
 import Sobre from './components/Sobre.jsx'
 import Contato from './components/Contato.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.render(
 
@@ -22,23 +23,18 @@ ReactDOM.render(
     <Topico id={"Projetos"} titulo={"Projetos"}></Topico>
     <div className="projetosBg">
     <Projetos imgStyle={"imgProjetos"} img1={img1} titulo={"Death Valley"} linkDownload={"https://andersonpaim.itch.io/death-valley"} linkCode={"https://github.com/AndersonPaim/DeathValley"} descricao={"Introdução: Jogo de ondas de inimigos para derrotar enquanto defende uma torre do ataque dos inimigos. \u000A Plataforma: PC  \u000A Engine: Unity  \u000A Linguagem de programação: C#  \u000A "} ></Projetos>
-    <Projetos imgStyle={"imgProjetos"} img1={img4} titulo={"CrazyBall"} linkDownload={"https://itch.io/queue/c/1454709/meus-projetos?game_id=951717"} linkCode={""} descricao={"Introdução: Jogo de baseball em realidade virtual onde o jogador é o rebatedor, com alguns puzzles para obter pontuação.  \u000A Plataforma: PC  \u000A Engine: Unity  \u000A Linguagem de programação: C#  \u000A "}></Projetos>
+    <Projetos imgStyle={"imgProjetos"} img1={img4} titulo={"CrazyBall"} linkDownload={"https://itch.io/queue/c/1454709/meus-projetos?game_id=951717"} linkCode={"https://itch.io/queue/c/1454709/meus-projetos?game_id=951717"} descricao={"Introdução: Jogo de baseball em realidade virtual onde o jogador é o rebatedor, com alguns puzzles para obter pontuação.  \u000A Plataforma: PC  \u000A Engine: Unity  \u000A Linguagem de programação: C#  \u000A "}></Projetos>
     <Projetos imgStyle={"imgProjetos"} img1={img7} titulo={"TankWar"} linkDownload={"https://itch.io/queue/c/1454709/meus-projetos?game_id=680096"} linkCode={"https://github.com/AndersonPaim/TankWar"} descricao={"Introdução: Jogo de combate de tanque com multijogador local que utiliza um controle alternativo desenvolvido com arduino.  \u000A Plataforma: PC  \u000A Engine: Unity + Arduino  \u000A Linguagem de programação: C# e C++ \u000A "}></Projetos>
-    <Projetos imgStyle={"imgProjetos"} img1={img13} titulo={"Projeto AR"} descricao={"Introdução: Jogo de cartas em realidade aumentado, onde o personagem da carta ganha vida na tela do celular, o jogo tem um combate em turnos com diferentes ataques. \u000A Plataforma: Mobile  \u000A Engine: Unity + Vuforia  \u000A Linguagem de programação: C# \u000A "}></Projetos>
-    <Projetos imgStyle={"imgProjetos2"} img1={img12} titulo={"Mais Um Dia"} linkDownload={"https://andersonpaim.github.io/MaisUmDia/"} linkCode={"https://github.com/AndersonPaim/MaisUmDia"} descricao={"Introdução: Jogo de corrida, com obstaculos pelo percurso o objetivo é chegar até o final, o jogo conta com sistema de login, banco de dados para save e uso de giroscópio para movimentação do player. \u000A Plataforma: Mobile  \u000A Engine: Framework Phaser3  \u000A Linguagem de programação: JavaScript \u000A "}></Projetos>
+    <Projetos imgStyle={"imgProjetos"} img1={img13} titulo={"Projeto AR"} linkDownload={"https://andersonpaim.itch.io/projeto-ar"} descricao={"Introdução: Jogo de cartas em realidade aumentado, onde o personagem da carta ganha vida na tela do celular, o jogo tem um combate em turnos com diferentes ataques. \u000A Plataforma: Mobile   \u000A Engine: Unity + Vuforia  \u000A Linguagem de programação: C# \u000A "}></Projetos>
+    <Projetos imgStyle={"imgProjetos2"} img1={img12} titulo={"Mais Um Dia"} linkDownload={"https://andersonpaim.itch.io/maisumdia"} linkCode={"https://github.com/AndersonPaim/MaisUmDia"} descricao={"Introdução: Jogo de corrida, com obstaculos pelo percurso o objetivo é chegar até o final, o jogo conta com sistema de login, banco de dados para save e uso de giroscópio para movimentação do player. \u000A Plataforma: Mobile  \u000A Engine: Framework Phaser3  \u000A Linguagem de programação: JavaScript \u000A "}></Projetos>
     </div>
-      {/*
-    <Projetos imgStyle={"projetosImg"} imgStyle2={"imgProjetos2"} img1={img1} img2={img2} img3={img3} titulo={"Death Valley"} descricao={"Introdução: Jogo de ondas de inimigos para derrotar enquanto defende uma torre dos ataque, o player possui diversas habilidades especiais para encarar esse desafio, sobreviva o quanto puder  \u000A Plataforma: PC  \u000A Engine: Unity  \u000A Linguagem de programação: C#  \u000A "}></Projetos>
-    <Projetos imgStyle={"projetosImg"} imgStyle2={"imgProjetos2"} img1={img4} img2={img5} img3={img6} titulo={"CrazyBall"} descricao={"Introdução: Jogo de baseball em realidade virtual onde o jogador é o rebatedor, visual cartoon e alguns puzzles para obter pontuação.  \u000A Plataforma: PC  \u000A Engine: Unity  \u000A Linguagem de programação: C#  \u000A "}></Projetos>
-    <Projetos imgStyle={"projetosImg"} imgStyle2={"imgProjetos2"} img1={img7} img2={img8} img3={img9} titulo={"TankWar"} descricao={"Introdução: Jogo de combate de tanque com multijogador local que utiliza um controle alternativo desenvolvido com arduino.  \u000A Plataforma: PC  \u000A Engine: Unity + Arduino  \u000A Linguagem de programação: C# e C++ \u000A "}></Projetos>
-    <Projetos imgStyle={"mudImg"} imgStyle2={"mudImg2"} img1={img10} img2={img11} img3={img12} titulo={"Mais Um Dia"} descricao={"Introdução: Jogo de corrida ponto a ponto com tematica de mobilidade urbana usando veiculos alternativos, com obstaculos pelo percurso o objetivo é chegar até o final, o jogo conta com sistema de login, banco de dados para save e uso de giroscópio para movimentação do player. \u000A Plataforma: Mobile  \u000A Engine: Framework Phaser3  \u000A Linguagem de programação: JavaScript \u000A "}></Projetos>
-      */}
+     
    
     <Topico id={"Sobre"} titulo={"Sobre"}></Topico>
-    <Sobre sobre={"Meu nome é Anderson Paim, tenho 20 anos, moro em  Gravataí-RS, sou estudante de Jogos Digitais na  Universidade do Vale do Rio dos Sinos(UNISINOS), estou na área de desenvolvimento de jogos e programação desde 2018, meu maior foco como desenvolvedor são de desenvolvimento de jogos na Unity e programação web front end."}/>
+    <Sobre sobre={"Meu nome é Anderson Paim, tenho 21 anos, moro em  Gravataí-RS, sou estudante de Jogos Digitais na  Universidade do Vale do Rio dos Sinos(UNISINOS), estou na área de desenvolvimento de jogos e programação desde 2018, meu maior foco como desenvolvedor são de desenvolvimento de jogos na Unity."}/>
     <Topico id={"Contato"} titulo={"Contato"}></Topico>
     <Contato></Contato>
-    <Topico titulo={""}></Topico>
+    <Footer></Footer>
   </div>,
   
     document.getElementById('root')
